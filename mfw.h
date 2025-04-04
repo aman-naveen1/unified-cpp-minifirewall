0835663783f932d5aaf213df5aa012ff1766aa70
Aman Naveen <aman.naveen@ramanujan.du.ac.in>
2025-04-04 14:24:37 +0530

    Update mfw.h

--- mfw.h
+++ mfw.h
@@ -1,45 +1,88 @@
-#ifndef MFW_H
-#define MFW_H
+#ifndef _MFW_H_
+#define _MFW_H_
 
+#include <stdint.h>
 #include <linux/types.h>
 
-#define DEVICE_PATH "/dev/minifw"
+#define DEVICE_INTF_NAME "/dev/mfw"
 
-struct mfw_rule {
-    int in;
-    uint8_t ip_version;
+// Protocols
+#ifndef IPPROTO_TCP
+#define IPPROTO_TCP 6
+#endif
 
-    union {
-        __u32 s_ip4;
-        __u8 s_ip6[16];
-    };
-    union {
-        __u32 s_mask4;
-        __u8 s_mask6[16];
-    };
-    __u16 s_port;
+#ifndef IPPROTO_UDP
+#define IPPROTO_UDP 17
+#endif
 
-    union {
-        __u32 d_ip4;
-        __u8 d_ip6[16];
-    };
-    union {
-        __u32 d_mask4;
-        __u8 d_mask6[16];
-    };
-    __u16 d_port;
-
-    __u8 proto;
+// Operation Modes
+enum {
+    MFW_NONE = 0,
+    MFW_ADD,
+    MFW_REMOVE,
+    MFW_VIEW
 };
 
+// Structure for a single firewall rule
+struct mfw_rule {
+    int8_t in;             // 1 = in, 0 = out
+    uint32_t s_ip;
+    uint32_t s_mask;
+    uint16_t s_port;
+    uint32_t d_ip;
+    uint32_t d_mask;
+    uint16_t d_port;
+    uint8_t proto;
+};
+
+// Instruction sent to kernel
 struct mfw_ctl {
-    int mode;
+    uint8_t mode;
     struct mfw_rule rule;
 };
 
-#define MFW_NONE    0
-#define MFW_ADD     1
-#define MFW_REMOVE  2
-#define MFW_VIEW    3
+#endif // _MFW_H_
+#ifndef _MFW_H_
+#define _MFW_H_
 
+#include <stdint.h>
+#include <linux/types.h>
+
+#define DEVICE_INTF_NAME "/dev/mfw"
+
+// Protocols
+#ifndef IPPROTO_TCP
+#define IPPROTO_TCP 6
 #endif
+
+#ifndef IPPROTO_UDP
+#define IPPROTO_UDP 17
+#endif
+
+// Operation Modes
+enum {
+    MFW_NONE = 0,
+    MFW_ADD,
+    MFW_REMOVE,
+    MFW_VIEW
+};
+
+// Structure for a single firewall rule
+struct mfw_rule {
+    int8_t in;             // 1 = in, 0 = out
+    uint32_t s_ip;
+    uint32_t s_mask;
+    uint16_t s_port;
+    uint32_t d_ip;
+    uint32_t d_mask;
+    uint16_t d_port;
+    uint8_t proto;
+};
+
+// Instruction sent to kernel
+struct mfw_ctl {
+    uint8_t mode;
+    struct mfw_rule rule;
+};
+
+#endif // _MFW_H_
